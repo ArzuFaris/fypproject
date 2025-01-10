565b5e29101b55b9b4b75207f8137342eae57891
ArzuFaris <arzu020329@gmail.com>
2025-01-10 16:16:14 +0800

    adding resource routes

--- project1/routes/web.php
+++ project1/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AcademicianController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\MilestoneController;
+use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/academicians',\App\Http\Controllers\AcademicianController::class);
+Route::resource('/grantprojects',\App\Http\Controllers\GrantProjectController::class);
+Route::resource('/milestones',\App\Http\Controllers\MilestoneController::class);
+//Route::resource('/staffs',\App\Http\Controllers\StaffController::class);
